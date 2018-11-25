f0fdd1a91c23d54383ce6392c1f4097b4ab49da5
ByteFan <docp@neodomaine.com>
2018-11-25 08:42:11 +0100

    A global page footer menu

---
+++ footer.php
@@ -0,0 +1,40 @@
+
+
+<center>
+<font size="-1">
+<a href="http://papabyte.com/byteball-sport-betting-bot.html"><img src="https://byteball.fr/papabytebanners-02.png"></a>
+<br><br>
+<a href="https://byteball.fr">Home</a> | 
+<a href="./Top100ByteballRichest.html">Top 100 richest</a> | 
+<a href="./byteballworldmap.php">Byteball world map</a> | 
+<a href="./stats.php">Witnesses monitoring page</a> | 
+<a href="./heartbeat.php">Byteball Statistics</a>
+</center>
+<br><br>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<center>
+New to Byteball ? Check <a href="http://www.byteball.org" target="_blank">www.byteball.org</a>
+&nbsp;also on Twitter <a href="https://twitter.com/ByteballOrg" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a>
+&nbsp;Bitcointalk <a href="https://bitcointalk.org/index.php?topic=1608859.0" target="_blank" title="BitcoinTalk thread"><i class="fa fa-bitcoin"></i></a>
+&nbsp;Medium <a href="https://medium.com/@Byteball" target="_blank" title="Medium"><i class="fa fa-medium"></i></a>
+&nbsp;Slack <a href="http://slack.byteball.org" target="_blank" title="Slack"><i class="fa fa-slack"></i></a>
+&nbsp;and Wiki <a href="https://wiki.byteball.org/" target="_blank" title="Wiki"><i class="fa fa-wikipedia-w"></i></a>
+</center>
+</font>
+<br><br>
+		
+		</div>
+		
+	</div>
+	
+	</div>
+	
+	<div id="sidebar">
+				
+		<a href="https://byteball.org" target="_blank"><img src="https://byteball.org/img/inphone/wallet.png?v2" height="230" width="120"></a>
+	
+	</div>
+		
+</body>
+
+</html>
